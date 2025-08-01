5c00d048511bbb1af65a84ac93531d0f83378eee
kpwcn <103888552+kpwcn@users.noreply.github.com>
2025-08-01 15:06:18 +0100

    Update Team.tsx

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -68,7 +68,7 @@ const Team = () => {
       </section>
 
       {/* Team Grid */}
-      <section className="py-16">
+      <section className="py-16 bg-white">
         <div className="max-w-container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
             {teamMembers.map((member, index) => (
